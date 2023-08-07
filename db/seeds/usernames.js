38ac7ddd8c389e7fe209f894f1aa2f838cc99460
Chandler Chilvers <cwchilvers@gmail.com>
2023-08-07 14:30:03 -0700

    Created seeds for users and thoughts.

---
+++ db/seeds/usernames.js
@@ -0,0 +1,13 @@
+module.exports = [
+    'leeroyjenkins',
+    'waifuhunter',
+    'JediMaster2001',
+    'NoobSlayerX',
+    'xXDarkShadowXx',
+    'DragonSlayer3000',
+    'gamerGoober',
+    'PCMasterRace',
+    'SniperMaster2000',
+    'ShadowNinja666',
+    'bob'
+];
